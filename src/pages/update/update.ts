a5dfce8f472d9dee2cd5f8ac2fdafd93867963dc
David Xu <cdavidxu@gmail.com>
2018-03-28 00:04:41 -0700

    update

--- src/pages/update/update.ts
+++ src/pages/update/update.ts
@@ -23,21 +23,64 @@ export class UpdatePage {
     console.log('ionViewDidLoad UpdatePage');
   }
 
-  athlete = {athleteName: '', athleteAward: ''}
-  coach = {coachName: '', coachAward: ''}
+  athlete = {athleteID: 0, athleteAward: '', athleteWeight: 0}
+  coach = {coachID: 0, coachAward: ''}
   award = {recipientName:'', awardType:''}
   participate = {seconds: '', length: '', stroke: '', name: '',pdate: '', ID:''}
 
 
   submitted = false;
-  Delete() {
+  DeleteAthlete() {
+
     this.submitted = true;
   }
 
 
   Insert() {
+
     this.submitted = true;
   }
+
+  Update(){
+
+    // check the initial projection of all athletes
+    var query = "select_star_from_athlete";
+    var phpURL = "http://www.ugrad.cs.ubc.ca/~j5m0b/clubsAwardsWon.php/" + query;
+
+    this.http.get(phpURL, {},{}).then(data => {
+      console.log(data.data);
+    })
+
+    //make the update to a specified athlete
+    var query = "update_athlete_set_weight_equal_" + this.athlete.athleteWeight +
+      "where_id_equal_" + this.athlete.athleteID;
+    var phpURL = "http://www.ugrad.cs.ubc.ca/~j5m0b/clubsAwardsWon.php/" + query;
+
+    this.http.get(phpURL, {},{}).then(data => {
+      console.log(data.data);
+    })
+
+    //check the projection of athlete to look for update
+    var query = "select_star_from_athlete";
+    var phpURL = "http://www.ugrad.cs.ubc.ca/~j5m0b/clubsAwardsWon.php/" + query;
+
+    this.http.get(phpURL, {},{}).then(data => {
+      console.log(data.data);
+    })
+
+    //select the updated athlete weight
+    var query = "select_weight_from_athlete_where_id_equal_" + this.athlete.athleteID;
+    var phpURL = "http://www.ugrad.cs.ubc.ca/~j5m0b/clubsAwardsWon.php/" + query;
+
+    this.http.get(phpURL, {},{}).then(data => {
+      console.log(data.data);
+    })
+
+
+
+    this.submitted = true;
+  }
+
 
   test() {
 
