6b3f926de177f7dda9012f5eb6c7fd7cdc9dad8a
Andrea (Andy) Park <andreapark15@hotmail.com>
2018-03-27 19:43:00 -0700

    bug

--- src/pages/athlete/athlete.module.ts
+++ src/pages/athlete/athlete.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { Athlete } from './athlete';
+import { AthletePage } from './athlete';
 
 @NgModule({
   declarations: [
