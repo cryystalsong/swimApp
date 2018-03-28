9d3c5a386034561ff9c46a2807d73df627648715
David Xu <cdavidxu@gmail.com>
2018-03-28 03:39:28 -0700

    Revert "trying to pull"
    This reverts commit e80e74f050df9ee7a99025a2e619d795dae09dea, reversing
    changes made to a12005fd4c41c1908f82c9e0049a85e8eb520646.

--- src/pages/update/update.ts
+++ src/pages/update/update.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+<<<<<<< HEAD
 import { HTTP } from '@ionic-native/http';
+=======
+import {MyApp} from "../../app/app.component";
+>>>>>>> parent of 6c918cc... might create merge conflicts with this
 
 /**
  * Generated class for the UpdatePage page.
@@ -25,8 +29,13 @@ export class UpdatePage {
 
   athlete = {athleteID: 0, athleteAward: '', athleteWeight: 0}
   coach = {coachID: 0, coachAward: ''}
+<<<<<<< HEAD
   award = {recipientPersonID: 0, recipientClubName: ''}
   participate = {seconds: 0, length: 0, stroke: '', name: '', pdate: '', ID:''}
+=======
+  award = {recipientName:'', awardType:''}
+  participate = {seconds: '', length: '', stroke: '', name: '',pdate: '', ID:''}
+>>>>>>> parent of 6c918cc... might create merge conflicts with this
 
 
   submitted = false;
@@ -69,7 +78,11 @@ export class UpdatePage {
     this.submitted = true;
   }
 
+<<<<<<< HEAD
   DeleteRecipientPerson(){
+=======
+  Update(){
+>>>>>>> parent of 6c918cc... might create merge conflicts with this
 
     var query = "delete from AwardPerson where id = " + this.award.recipientPersonID;
     var phpURL = "http://www.ugrad.cs.ubc.ca/~x1p0b/clubsAwardsWon.php?q=" + query;
@@ -104,6 +117,7 @@ export class UpdatePage {
 
     })
 
+<<<<<<< HEAD
   }
 
 
@@ -127,7 +141,60 @@ export class UpdatePage {
       })
 
     })
+=======
+    //make the update to a specified athlete
+    var query = "update athlete set weight = " + this.athlete.athleteWeight +
+      " where id = " + this.athlete.athleteID;
+    // var phpURL = "http://www.ugrad.cs.ubc.ca/~u2o0b/clubsAwardsWon.php?q=";
+    //
+    // this.http.get(phpURL+query, {},{}).then(data => {
+    //   console.log(query);
+    //
+    //   var query1 = "select * from athlete";
+    //
+    //   this.http.get(phpURL+query1, {},{}).then(data => {
+    //     console.log(data.data);
+    //   });
 
+    // });
+
+    console.log(this.myApp.retrieveQueryData(query));
+
+
+
+
+
+
+    //check the projection of athlete to look for update
+
+
+    // //select the updated athlete weight
+    // var query2 = "select_weight_from_athlete_where_id_equal_" + this.athlete.athleteID;
+    //
+    // this.http.get(phpURL+query2, {},{}).then(data => {
+    //   console.log(data.data);
+    // })
+
+    //
+    //
+    //
+    // this.submitted = true;
+  }
+
+
+  test() {
+
+    var query = "select * from person";
+    // var phpURL = "http://www.ugrad.cs.ubc.ca/~j5m0b/clubsAwardsWon.php/" + query;
+    //
+    // this.http.get(phpURL, {},{}).then(data => {
+    //   console.log(data.data);
+    // })
+
+    console.log(query);
+>>>>>>> parent of 6c918cc... might create merge conflicts with this
+
+    console.log(this.myApp.retrieveQueryData(query));
 
   }
 
@@ -151,5 +218,6 @@ export class UpdatePage {
     })
     this.submitted = true;
   }
+
 
 }
