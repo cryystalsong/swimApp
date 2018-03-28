e6ca2ac3272363e45d15693745fd04775fc36ddd
David Xu <cdavidxu@gmail.com>
2018-03-28 03:41:33 -0700

    work?

--- src/pages/update/update.ts
+++ src/pages/update/update.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
 import { HTTP } from '@ionic-native/http';
-=======
-import {MyApp} from "../../app/app.component";
->>>>>>> parent of 6c918cc... might create merge conflicts with this
 
 /**
  * Generated class for the UpdatePage page.
@@ -29,13 +25,8 @@ export class UpdatePage {
 
   athlete = {athleteID: 0, athleteAward: '', athleteWeight: 0}
   coach = {coachID: 0, coachAward: ''}
-<<<<<<< HEAD
   award = {recipientPersonID: 0, recipientClubName: ''}
   participate = {seconds: 0, length: 0, stroke: '', name: '', pdate: '', ID:''}
-=======
-  award = {recipientName:'', awardType:''}
-  participate = {seconds: '', length: '', stroke: '', name: '',pdate: '', ID:''}
->>>>>>> parent of 6c918cc... might create merge conflicts with this
 
 
   submitted = false;
@@ -53,9 +44,9 @@ export class UpdatePage {
       this.http.get(phpURL, {},{}).then(data => {
         console.log(data.data);
       })
+
     })
 
-    this.submitted = true;
   }
 
   DeleteCoach(){
@@ -75,14 +66,9 @@ export class UpdatePage {
 
     })
 
-    this.submitted = true;
   }
 
-<<<<<<< HEAD
   DeleteRecipientPerson(){
-=======
-  Update(){
->>>>>>> parent of 6c918cc... might create merge conflicts with this
 
     var query = "delete from AwardPerson where id = " + this.award.recipientPersonID;
     var phpURL = "http://www.ugrad.cs.ubc.ca/~x1p0b/clubsAwardsWon.php?q=" + query;
@@ -117,17 +103,14 @@ export class UpdatePage {
 
     })
 
-<<<<<<< HEAD
   }
-
-
 
   Insert() {
 
     var query = "insert into Participate values (" + this.participate.seconds + ", "
-    + this.participate.length + ", " + "'" + this.participate.stroke + "'" + ", "
-    + "'" + this.participate.name + "'" + ", " + "'" + this.participate.pdate + "'" + ", "
-    + this.participate.ID + ")";
+      + this.participate.length + ", " + "'" + this.participate.stroke + "'" + ", "
+      + "'" + this.participate.name + "'" + ", " + "'" + this.participate.pdate + "'" + ", "
+      + this.participate.ID + ")";
     var phpURL = "http://www.ugrad.cs.ubc.ca/~x1p0b/clubsAwardsWon.php?q=" + query;
 
     this.http.get(phpURL, {},{}).then(data => {
@@ -141,67 +124,13 @@ export class UpdatePage {
       })
 
     })
-=======
-    //make the update to a specified athlete
-    var query = "update athlete set weight = " + this.athlete.athleteWeight +
-      " where id = " + this.athlete.athleteID;
-    // var phpURL = "http://www.ugrad.cs.ubc.ca/~u2o0b/clubsAwardsWon.php?q=";
-    //
-    // this.http.get(phpURL+query, {},{}).then(data => {
-    //   console.log(query);
-    //
-    //   var query1 = "select * from athlete";
-    //
-    //   this.http.get(phpURL+query1, {},{}).then(data => {
-    //     console.log(data.data);
-    //   });
-
-    // });
-
-    console.log(this.myApp.retrieveQueryData(query));
-
-
-
-
-
-
-    //check the projection of athlete to look for update
-
-
-    // //select the updated athlete weight
-    // var query2 = "select_weight_from_athlete_where_id_equal_" + this.athlete.athleteID;
-    //
-    // this.http.get(phpURL+query2, {},{}).then(data => {
-    //   console.log(data.data);
-    // })
-
-    //
-    //
-    //
-    // this.submitted = true;
-  }
-
-
-  test() {
-
-    var query = "select * from person";
-    // var phpURL = "http://www.ugrad.cs.ubc.ca/~j5m0b/clubsAwardsWon.php/" + query;
-    //
-    // this.http.get(phpURL, {},{}).then(data => {
-    //   console.log(data.data);
-    // })
-
-    console.log(query);
->>>>>>> parent of 6c918cc... might create merge conflicts with this
-
-    console.log(this.myApp.retrieveQueryData(query));
 
   }
 
   Updated(){
     //make the update to a specified athlete
     var query = "update athlete set weight = " + this.athlete.athleteWeight + " where id = "
-    + this.athlete.athleteID;
+      + this.athlete.athleteID;
     var phpURL = "http://www.ugrad.cs.ubc.ca/~x1p0b/clubsAwardsWon.php?q=" + query;
 
     this.http.get(phpURL, {},{}).then(data => {
@@ -214,10 +143,8 @@ export class UpdatePage {
         console.log(data.data);
       })
 
-
     })
-    this.submitted = true;
-  }
 
+  }
 
 }
