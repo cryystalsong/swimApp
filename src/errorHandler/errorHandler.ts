48fc00a53b2a80317fff9983838819b9a1dc01d5
jchen4120 <jenniferchen@alumni.ubc.ca>
2018-03-29 12:09:52 -0700

    Revert "mult selects"
    This reverts commit 6c11b324e613ef4b8b5f918abb25dec07749b3c8.

---
+++ src/errorHandler/errorHandler.ts
@@ -0,0 +1,36 @@
+import { ErrorHandler, Injectable} from '@angular/core';
+import {AlertController} from "ionic-angular";
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+
+  constructor(private alertCtrl: AlertController) {
+
+  }
+  // alert = this.alertCtrl.create({
+  //   title: 'ERROR!',
+  //   subTitle: 'Incorrect information',
+  //   buttons: ['Dismiss']
+  // });
+  // let alert = this.alertCtrl.create({
+  //   title: 'Low battery',
+  //   subTitle: '10% of battery remaining',
+  //   buttons: ['Dismiss']
+  // });
+  // alert.present();
+
+
+  handleError(error) {
+    console.log('Errrrrrrrror ' + error);
+    let alert = this.alertCtrl.create({
+      title: 'ERROR!',
+      subTitle: 'Incorrect information',
+      buttons: ['Dismiss']
+    });
+     alert.present();
+
+    // IMPORTANT: Rethrow the error otherwise it gets swallowed
+
+  }
+
+}
