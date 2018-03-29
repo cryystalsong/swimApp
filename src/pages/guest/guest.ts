c56149bc0de64671fc3c15f979dac5ef2135a9f9
cryystalsong <cryystalsong@gmail.com>
2018-03-29 13:13:36 -0700

    added input boxes for athlete + coaches

--- src/pages/guest/guest.ts
+++ src/pages/guest/guest.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ModalController, IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
 // import {AthletePage} from "../athlete/athlete";
 // import {AdminPage} from "../admin/admin";
@@ -17,10 +17,9 @@ export class GuestPage {
               public modalCtrl: ModalController,
               public navParams: NavParams,
               public myApp: MyApp,
-              private alertCtrl: AlertController
-              ) {
+              private alertCtrl: AlertController) {
     var fromPage = navParams.get('id');
-    if(fromPage !== "login") {
+    if (fromPage !== "login") {
       this.hideUpdate = true;
     }
   }
@@ -56,14 +55,14 @@ export class GuestPage {
     } else if (this.club.clubOptionSelected === "clubCoaches") {
       q = "select * from person p, coach c where p.id=c.id and c.id in (select b.id from belongs b where b.clubname = '" + this.club.clubName + "')";
     } else {
-      q= "select c.name, c.address from club c where c.name = '" + this.club.clubName + "'";
+      q = "select c.name, c.address from club c where c.name = '" + this.club.clubName + "'";
     }
 
 
-    this.myApp.retrieveQueryData(q).then((data)=> {
+    this.myApp.retrieveQueryData(q).then((data) => {
       this.myApp.displayQueryData(data, "clubResult");
-        console.log(data);
-      });
+      console.log(data);
+    });
     //   .catch((err)=>{
     // this.presentAlert();
     // });
@@ -73,7 +72,11 @@ export class GuestPage {
   }
 
   athlete = {
-    allAthletes: false, athlName: '', height: '', show: {
+    allAthletes: false,
+    athlName: '',
+    height: '',
+    sHeight: '',
+    show: {
       name: true,
       id: false,
       sex: false,
@@ -84,6 +87,7 @@ export class GuestPage {
       country: false
     }
   };
+
   onAthleteSubmit() {
     var query = '';
     // console.log("athlete.athlAward " + this.athlete.athlAward);
@@ -92,7 +96,7 @@ export class GuestPage {
     console.log("athlete.show.height " + this.athlete.show.height);
     this.submitted = true;
     let select = "select ";
-    let from = "from athlete a, person p" ;
+    let from = "from athlete a, person p";
     let where = "where a.id = p.id ";
     let flag = 0;
 
@@ -159,8 +163,10 @@ export class GuestPage {
       }
     }
 
-   if (this.athlete.athlName) {
+    if (this.athlete.athlName) {
       where += " and p.name = \'" + this.athlete.athlName + "\'";
+    } else if (this.athlete.sHeight) {
+      where += " and " + "a.height =" + this.athlete.sHeight;
     } else if (this.athlete.height) {
       where += " and " + "a.height in (select " + this.athlete.height + "from athlete a1)";
     }
@@ -171,7 +177,7 @@ export class GuestPage {
 
     console.log("query " + query);
 
-    this.myApp.retrieveQueryData(query).then((data)=> {
+    this.myApp.retrieveQueryData(query).then((data) => {
       this.myApp.displayQueryData(data, "athleteResult");
       console.log(data);
     });
@@ -196,6 +202,7 @@ export class GuestPage {
     allCoaches: false,
     coachName: '',
     yrs: '',
+    sYrs: '',
     show: {
       name: true,
       id: false,
@@ -207,12 +214,13 @@ export class GuestPage {
       // awards: false
     }
   };
+
   onCoachSubmit() {
     var query = '';
     console.log(this.coach.show.name);
     this.submitted = true;
     let select = "select ";
-    let from = "from coach c, person p" ;
+    let from = "from coach c, person p";
     let where = "where c.id = p.id";
     let flag = 0;
     if (this.coach.show.name) {
@@ -260,9 +268,11 @@ export class GuestPage {
       }
     }
 
-      if (this.coach.coachName) {
+    if (this.coach.coachName) {
       where += " and p.name = \'" + this.coach.coachName + "\'";
-    } else if (this.coach.yrs){
+    } else if (this.coach.sYrs) {
+      where += " and c.yearsOfExp > " + this.coach.sYrs;
+    } else if (this.coach.yrs) {
       where += " and c.yearsOfExp = (select " + this.coach.yrs + " from Coach c1)";
     }
 
@@ -270,7 +280,7 @@ export class GuestPage {
 
 
     console.log("query " + query);
-    this.myApp.retrieveQueryData(query).then((data)=> {
+    this.myApp.retrieveQueryData(query).then((data) => {
       this.myApp.displayQueryData(data, "coachResult");
       console.log(data);
     });
@@ -299,12 +309,12 @@ export class GuestPage {
     var q = '';
 
     if (this.comp.compOptionSelected === "compTitleHolders") {
-      q="select e.titleholder, e.length, e.stroke from events e where e.cname = '" + this.comp.compName + "'"
+      q = "select e.titleholder, e.length, e.stroke from events e where e.cname = '" + this.comp.compName + "'"
     } else {
-      q="select p.name, pa.length, pa.stroke, pa.length from person p, participate pa where p.id = pa.id and pa.name = '" + this.comp.compName + "'"
+      q = "select p.name, pa.length, pa.stroke, pa.length from person p, participate pa where p.id = pa.id and pa.name = '" + this.comp.compName + "'"
     }
 
-    this.myApp.retrieveQueryData(q).then((data)=> {
+    this.myApp.retrieveQueryData(q).then((data) => {
       this.myApp.displayQueryData(data, "compResult");
       console.log(data);
     });
@@ -319,7 +329,7 @@ export class GuestPage {
   divisionQuery() {
     var q = "select p.name from person p, athlete a where p.id=a.id and NOT EXISTS ((select c.name from competition c) MINUS (select p.name from participate p where p.id=a.id))"
 
-    this.myApp.retrieveQueryData(q).then((data)=> {
+    this.myApp.retrieveQueryData(q).then((data) => {
       this.myApp.displayQueryData(data, "otherResult");
       console.log(data);
     });
@@ -329,9 +339,9 @@ export class GuestPage {
   }
 
   minNestedAggQuery() {
-    var q ="select * from (select avg(seconds) as avgSeconds, stroke, length from participate group by stroke, length) where avgSeconds in (select min(avgSecondsTwo) from (select avg(seconds) as avgSecondsTwo from participate group by stroke, length))";
+    var q = "select * from (select avg(seconds) as avgSeconds, stroke, length from participate group by stroke, length) where avgSeconds in (select min(avgSecondsTwo) from (select avg(seconds) as avgSecondsTwo from participate group by stroke, length))";
 
-    this.myApp.retrieveQueryData(q).then((data)=> {
+    this.myApp.retrieveQueryData(q).then((data) => {
       this.myApp.displayQueryData(data, "otherResult");
       console.log(data);
     });
@@ -343,7 +353,7 @@ export class GuestPage {
   maxNestedAggQuery() {
     var q = "select * from (select avg(seconds) as avgSeconds, stroke, length from participate group by stroke, length) where avgSeconds in (select max(avgSecondsTwo) from (select avg(seconds) as avgSecondsTwo from participate group by stroke, length))";
 
-    this.myApp.retrieveQueryData(q).then((data)=> {
+    this.myApp.retrieveQueryData(q).then((data) => {
       this.myApp.displayQueryData(data, "otherResult");
       console.log(data);
     });
@@ -351,11 +361,13 @@ export class GuestPage {
     // this.presentAlert();
     // });
   }
+
   coachName = "";
+
   coachAthletes() {
     var q = "select ap.name from person ap, coaches co where co.AID = ap.ID AND co.CID NOT IN ((SELECT co2.CID FROM " +
       "Coaches co2) MINUS (SELECT c.ID FROM Coach c, Person cp WHERE cp.name = \'" + this.coachName + "\' and cp.ID = c.ID))";
-    this.myApp.retrieveQueryData(q).then((data)=> {
+    this.myApp.retrieveQueryData(q).then((data) => {
       this.myApp.displayQueryData(data, "otherResult");
       console.log(data);
     });
