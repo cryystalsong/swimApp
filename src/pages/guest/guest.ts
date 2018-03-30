df3bc75278e83a4f4c8183f0947a77b4806319d1
Andrea (Andy) Park <andreapark15@hotmail.com>
2018-03-29 17:09:10 -0700

    others

--- src/pages/guest/guest.ts
+++ src/pages/guest/guest.ts
@@ -464,26 +464,21 @@ export class GuestPage {
   }
   // select ap.AwardName, ap.year from AwardPerson ap, Coach c, Person p where p.name = 'Keith Turner' and c.ID = p.ID and ap.ID = c.ID
 
-  others = {allComps: false, maxAvg: false, minAvg: false, coachedBy: false, coachAwards: false};
-
+  others = "";
+  
   onOthersSubmit() {
-    if (this.others.allComps) {
+    if (this.others == "allComps") {
       this.divisionQuery();
-    } else if (this.others.maxAvg) {
+    } else if (this.others == "maxAvg") {
       this.maxNestedAggQuery();
-    }else if (this.others.minAvg) {
+    }else if (this.others == "minAvg") {
       this.minNestedAggQuery();
-    } else if (this.others.coachedBy) {
+    } else if (this.others == "coachedBy") {
       this.coachAthletes();
-    } else if (this.others.coachAwards) {
+    } else if (this.others == "coachAwards") {
       this.coachAwards();
     }
     console.log('others submit' + JSON.stringify(this.others));
-  }
-
-  onOthersChange(input : any) {
-    this.others = {allComps: false, maxAvg: false, minAvg: false, coachedBy: false, coachAwards: false};
-
   }
 
 
